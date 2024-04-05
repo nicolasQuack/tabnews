d777bc56209a4fc0c66ed2773ef967f628451412
nicolasQuack <nicolas.quack@icloud.com>
2024-04-05 09:25:53 -0300

    style(pages/): change home text;

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Eu vou conseguir cuprir com esse curso do Filipe Deschamps.</h1>;
+  return (
+    <>
+      <h1>Eu me chamo Nicolas Felipe.</h1>
+      <p>Eu vou conseguir cuprir com esse curso do Filipe Deschamps.</p>
+    </>
+  );
 }
 
 export default Home;
