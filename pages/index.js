436819d1da06124ec2ef1825f27e59296bab9295
nicolasQuack <nicolas.quack@icloud.com>
2024-04-15 17:45:09 -0300

    fix(pages/): fix spelling mistake in first paragraph;

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <h1>Eu me chamo Nicolas Felipe.</h1>
-      <p>Eu vou conseguir cuprir com esse curso do Filipe Deschamps.</p>
+      <p>Eu vou conseguir cumprir com esse curso do Filipe Deschamps.</p>
     </>
   );
 }
