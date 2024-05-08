e26afa2c3d79f70f122ce9faa3accaf5245f3ea0
nicolasQuack <nicolas.quack@icloud.com>
2024-05-08 20:42:56 -0300

    chore(*): finish `/api/v1/status` endpoint;

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+
+  const dbVersion = await database.query(`SHOW server_version;`);
+  const dbVersionRow = dbVersion.rows[0].server_version;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbCurrentConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const dbCurrentConnectionsValue = dbCurrentConnectionsResult.rows[0].count;
+
+  const dbMaxConnectionsResult = await database.query(`SHOW max_connections;`);
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersionRow,
+        connections: {
+          current_connections: dbCurrentConnectionsValue,
+          max_connections: parseInt(dbMaxConnectionsValue),
+        },
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET `/api/v1/status`should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  // date is valid
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  // database version is valid
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // current connections is valid
+  expect(
+    responseBody.dependencies.database.connections.current_connections,
+  ).toEqual(1);
+
+  // max connections is valid
+  expect(
+    responseBody.dependencies.database.connections.max_connections,
+  ).toEqual(100);
 });
