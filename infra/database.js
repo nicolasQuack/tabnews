f3d38a052ec9e7afa22e804fc3247e63ef6031f7
nicolasQuack <nicolas.quack@icloud.com>
2024-05-09 00:13:12 -0300

    chore(infra/): handles database `ssl` connection;

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,18 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
-    await client.end();
+    // await client.end();
   }
 }
 
